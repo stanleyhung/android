7e0d0ba856b28ed31636dc21c37b84b1578c65a7
stanleyhung <stanley.hung25@berkeley.edu>
2014-05-01 14:28:16 -0700

    added server to manage networking

--- PacketControl/src/packetControl/Control.java
+++ PacketControl/src/packetControl/Control.java
@@ -2,9 +2,11 @@ package packetControl;
 
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.util.concurrent.ForkJoinPool;
 
 public class Control {
 	private RobotControl robot;
+	private Server server;
 	
 	public Control() {
 		try {
@@ -15,12 +17,19 @@ public class Control {
 			e.printStackTrace();
 		}
 	}
-	/**
+	
+	public void start() throws InterruptedException {
+		server.start();
+		server.join();
+	}
+	
+	/**w
 	 * @param args
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException {
+		Control c = new Control();
+		c.start();
 	}
 
 }
---
+++ PacketControl/src/packetControl/Server.java
@@ -0,0 +1,7 @@
+package packetControl;
+
+public class Server extends Thread {
+	public void run() {
+		
+	}
+}

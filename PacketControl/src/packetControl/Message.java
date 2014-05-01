11938ce731bade3442ef0921d7cce56b193dd08f
stanleyhung <stanley.hung25@berkeley.edu>
2014-05-01 14:03:56 -0700

    added Message class

--- PacketControl/src/packetControl/Message.java
+++ PacketControl/src/packetControl/Message.java
@@ -1,19 +1,33 @@
 package packetControl;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 //Class that enables communication between Control and Robot Control
 public class Message {
-
+	public String myMessage;
+	
 	final static String STOP = "stop";
 	final static String PLAY = "play";
 	final static String PAUSE = "pause";
 	final static String NEXT = "next";
 	final static String PREVIOUS = "previous";
 	
-	private static final ArrayList<String> validCommands;
+	private static final Map<String, Boolean> validCommands;
 	static {
-		
+		validCommands = new HashMap<String, Boolean>();
+		validCommands.put(STOP, true);
+		validCommands.put(PLAY, true);
+		validCommands.put(PAUSE, true);
+		validCommands.put(NEXT, true);
+		validCommands.put(PREVIOUS, true);
+	}
+	
+	public Message(String message) {
+		if(!validCommands.containsKey(myMessage)) {
+			throw new IllegalArgumentException();
+		}
+		myMessage = message;
 	}
 	/**
 	 * @param args

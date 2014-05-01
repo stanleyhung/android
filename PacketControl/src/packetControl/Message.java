4a434f579e2db324b4c10c69528cc851a5232874
stanleyhung <stanley.hung25@berkeley.edu>
2014-05-01 13:58:58 -0700

    added Message class

---
+++ PacketControl/src/packetControl/Message.java
@@ -0,0 +1,26 @@
+package packetControl;
+
+import java.util.ArrayList;
+
+//Class that enables communication between Control and Robot Control
+public class Message {
+
+	final static String STOP = "stop";
+	final static String PLAY = "play";
+	final static String PAUSE = "pause";
+	final static String NEXT = "next";
+	final static String PREVIOUS = "previous";
+	
+	private static final ArrayList<String> validCommands;
+	static {
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

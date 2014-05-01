2cea5c734cd80623542f571ab6c8160b630d0dff
stanleyhung <stanley.hung25@berkeley.edu>
2014-05-01 14:11:59 -0700

    added handle method to press keys

--- PacketControl/src/packetControl/RobotControl.java
+++ PacketControl/src/packetControl/RobotControl.java
@@ -2,7 +2,10 @@ package packetControl;
 
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.event.KeyEvent;
 
+
+//RobotControl handles the lower-level logic for interfacing with VLC Media Player through keystrokes
 class RobotControl extends Robot {
 	private Robot robot;
 	
@@ -12,6 +15,19 @@ class RobotControl extends Robot {
 	/**
 	 * @param args
 	 */
+	
+	public int handle(Message m) {
+		try {
+			robot.keyPress(m.getKey());
+			robot.keyRelease(m.getKey());
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			return -1;
+		}
+		return 0;
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

9b40c3b9aa4aaa9a1fe4ed31b20bcb6df3e89f0f
stanleyhung <stanley.hung25@berkeley.edu>
2014-05-01 14:35:23 -0700

    added request queue for server

--- PacketControl/src/packetControl/Server.java
+++ PacketControl/src/packetControl/Server.java
@@ -1,9 +1,29 @@
 package packetControl;
 
+import java.awt.AWTException;
+import java.util.LinkedList;
+
+
 public class Server extends Thread {
 	private RobotControl robot;
+	private LinkedList<Message> requests;
 	
 	public void run() {
-		
+		try {
+			robot = new RobotControl();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+		requests = new LinkedList<Message>();
+	}
+	
+	private synchronized void addToQueue(Message m) {
+		requests.add(m);
+	}
+	
+	private synchronized Message getNext() {
+		return requests.poll();
 	}
 }

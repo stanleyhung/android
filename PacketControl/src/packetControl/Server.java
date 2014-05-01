b0ffdae0dee653957d7f26f16e189953ea3721a5
stanleyhung <stanley.hung25@berkeley.edu>
2014-05-01 14:41:47 -0700

    removed Control and put all functionality in server

--- PacketControl/src/packetControl/Server.java
+++ PacketControl/src/packetControl/Server.java
@@ -2,21 +2,25 @@ package packetControl;
 
 import java.awt.AWTException;
 import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class Server extends Thread {
+	private final static int NUM_THREADS = 4;
 	private RobotControl robot;
 	private LinkedList<Message> requests;
+	private ExecutorService executor;
 	
-	public void run() {
+	public Server() {
 		try {
 			robot = new RobotControl();
 		} catch (AWTException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return;
 		}
 		requests = new LinkedList<Message>();
+		executor = Executors.newFixedThreadPool(NUM_THREADS);
 	}
 	
 	private synchronized void addToQueue(Message m) {

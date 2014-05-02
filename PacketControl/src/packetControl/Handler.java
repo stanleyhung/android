520d49a807ba51560b092a3bdb93c6b19d0fde79
stanleyhung <stanley.hung25@berkeley.edu>
2014-05-02 10:45:20 -0700

    added Handler and options to turn Network on/off

---
+++ PacketControl/src/packetControl/Handler.java
@@ -0,0 +1,11 @@
+package packetControl;
+
+public class Handler implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- PacketControl/src/packetControl/Network.java
+++ PacketControl/src/packetControl/Network.java
@@ -3,6 +3,11 @@
  */
 package packetControl;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
 /**
  * @author Stanley
  *
@@ -10,16 +15,39 @@ package packetControl;
 public class Network implements Runnable {
 	
 	private final static int PORT = 9;
+	private ServerSocket server;
+	LinkedList<Socket> clients;
+	private boolean status;
 	
-	public Network() {
+	public Network() throws IOException {
+		server = new ServerSocket(PORT);
+		clients = new LinkedList<Socket>();
+		status = false;
+	}
+	
+	//turns the server on and returns true on success, false if the server was already on
+	public synchronized boolean turnOn() {
+		if (status == true) {
+			return false;
+		}
+		status = true;
+		return true;
+	}
+	
+	//turns the server off and returns true on success, false if the server was already off
+	public synchronized boolean turnOff() {
+		if (status == false) {
+			return false;
+		}
+		status = false;
+		return false;
 	}
 	/* (non-Javadoc)
 	 * @see java.lang.Runnable#run()
 	 */
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }

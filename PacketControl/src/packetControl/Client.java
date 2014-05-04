0dc12cfa3ad7c9e67314493198f535e9e19b1901
stanleyhung <stanley.hung25@berkeley.edu>
2014-05-03 23:48:21 -0700

    added client class

---
+++ PacketControl/src/packetControl/Client.java
@@ -0,0 +1,19 @@
+package packetControl;
+
+public class Client implements Runnable {
+
+	@Override
+	public void run() {
+		//Test the Network and Handler classes by simulating a client
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

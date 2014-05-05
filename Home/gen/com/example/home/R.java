00bb3fc88da696916a24ed2a0d5999f901057955
stanleyhung <stanley.hung25@berkeley.edu>
2014-05-04 21:52:39 -0700

    fixed stupid default

--- Home/gen/com/example/home/R.java
+++ Home/gen/com/example/home/R.java
@@ -24,9 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080009;
+        public static final int button_wake_desc=0x7f080001;
         public static final int myButton=0x7f080000;
-        public static final int myText=0x7f080001;
+        public static final int nextButton=0x7f080006;
+        public static final int pauseButton=0x7f080005;
+        public static final int playButton=0x7f080004;
+        public static final int previousButton=0x7f080007;
+        public static final int quitButton=0x7f080008;
+        public static final int startButton=0x7f080002;
+        public static final int stopButton=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,7 +45,9 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int button_wake=0x7f050002;
-        public static final int myText=0x7f050003;
+        public static final int button_wake_desc=0x7f050003;
+        public static final int off_string=0x7f050005;
+        public static final int start_remote=0x7f050004;
     }
     public static final class style {
         /** 
